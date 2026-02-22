f6026d8d7af08ccb539aed3ec0163ebf30797d5d
Amanhost <amanprakash1639@gmail.com>
2026-02-22 19:26:30 +0530

    email features integrated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import Nav from "./component/Nav";
 import "./style.css";
+import WhatsAppButton from "./component/ChatBoat/WhatsAppButton";
 console.log();
 const App = () => {
   return (
     <Router>
       <div className="App">
         <Nav />
+        <WhatsAppButton />
       </div>
     </Router>
   );
